e1d2745324ef3e465fda272736d19e59fa49d664
HufflePuffProgrammer <wdrum4food@yahoo.com>
2019-01-05 12:04:54 -0800

    Updates

---
+++ precache-manifest.2deeef402425ff95f8c8afd98ba0f6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "bbac8e175df737d7eb2b",
+    "url": "/contactmanager/static/js/main.bbac8e17.chunk.js"
+  },
+  {
+    "revision": "47b66c76c7893851715a",
+    "url": "/contactmanager/static/js/1.47b66c76.chunk.js"
+  },
+  {
+    "revision": "bbac8e175df737d7eb2b",
+    "url": "/contactmanager/static/css/main.b9cee727.chunk.css"
+  },
+  {
+    "revision": "47b66c76c7893851715a",
+    "url": "/contactmanager/static/css/1.5c62bf8f.chunk.css"
+  },
+  {
+    "revision": "26a8ac4073ab00491fba091fa2a75198",
+    "url": "/contactmanager/index.html"
+  }
+];
